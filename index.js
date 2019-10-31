f1ec28162006d9efc15c5afd434a24fb49af4eb1
zlxfy <56485461+zlxfy@users.noreply.github.com>
2019-10-31 18:30:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
-var token = process.env.TOKEN;
+const TOKEN = process.env.TOKEN;
 var prefix = "/"
 var eightball = [ // sets the answers to an eightball
     "Ouiiii ",
