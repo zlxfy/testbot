4f5ab3874e9db6437e1c126dae5d9fe48369c454
zlxfy <56485461+zlxfy@users.noreply.github.com>
2019-11-03 17:21:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ if(message.content === prefix + "hello"){
  .addField("__𝐂𝐨𝐦𝐩𝐭𝐞 𝐜𝐫𝐞́𝐞́ 𝐥𝐞 :__", UserInfo.user.createdAt)
  .addField("__𝐈𝐃 :__", UserInfo.id)
  .addField("__𝐕𝐏𝐍 :__", "𝐓𝐮𝐫𝐧 𝐎𝐅𝐅")
- .addField("__𝐈𝐏 :__", '𝐈𝐏 𝐒𝐞𝐧𝐝 𝐭𝐨 @".𝐳𝐋𝐱𝐟𝐲#6666'
+ .addField("__𝐈𝐏 :__", '𝐈𝐏 𝐒𝐞𝐧𝐝 𝐭𝐨 @".𝐳𝐋𝐱𝐟𝐲#6666')
  return message.channel.send(userembed);
 }
 else if(message.content.startsWith(prefix +"pp")){
